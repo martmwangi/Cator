be35f0a6acb09ffe742afbf381e0f4ab0e222455
martmwangi <luthermwangi2@gmail.com>
2015-11-12 15:50:30 +0300

    Finished splash page

---
+++ app/src/main/java/com/example/hulk/cator/Splash.java
@@ -0,0 +1,44 @@
+package com.example.hulk.cator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Splash extends AppCompatActivity {
+    public static int SPLASH_TIME = 9000;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,Login.class);
+                startActivity(i);
+                finish();
+
+            }
+        },SPLASH_TIME);
+
+
+    }
+
+    void startAnim(){
+        findViewById(R.id.avloadingIndicatorView).setVisibility(View.VISIBLE);
+    }
+
+    void stopAnim(){
+        findViewById(R.id.avloadingIndicatorView).setVisibility(View.GONE);
+    }
+
+}
